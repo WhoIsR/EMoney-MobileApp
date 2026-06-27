6d4ab17d00ca02f07513326e52380bf3649a2957
radja <radjasatrio70@gmail.com>
2026-06-27 14:44:16 +0700

    style: tambah komponen glass card
    Menambahkan surface reusable untuk tampilan glassmorphism.

---
+++ lib/presentation/widgets/glass_card.dart
@@ -0,0 +1,45 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import '../../core/theme/app_colors.dart';
+
+class GlassCard extends StatelessWidget {
+  final Widget child;
+  final EdgeInsetsGeometry padding;
+  final double radius;
+  final Color color;
+  final Border? border;
+  final List<BoxShadow>? boxShadow;
+
+  const GlassCard({
+    super.key,
+    required this.child,
+    this.padding = const EdgeInsets.all(16),
+    this.radius = 24,
+    this.color = AppColors.glass,
+    this.border,
+    this.boxShadow,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(radius),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 18, sigmaY: 18),
+        child: Container(
+          padding: padding,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(radius),
+            border: border ??
+                Border.all(color: AppColors.glassLine, width: 1.1),
+            boxShadow: boxShadow ?? AppColors.shadowSoft,
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
